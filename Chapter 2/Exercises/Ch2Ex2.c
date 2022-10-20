9533d6c8cd7cbd83a6383763f97f0400719b8466
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2022-10-20 18:00:00 -0500

    Create Ch2Ex2.c

---
+++ Chapter 2/Exercises/Ch2Ex2.c
@@ -0,0 +1,40 @@
+/*
+  Section 2.2
+  Consider the following program:
+  
+  #include <stdio.h>
+  
+  int main(void) 
+  {
+    printf("Parkinson's Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+  }
+  
+    (a) Identify the directives and statements in the program.
+    (b) What output does the program produce.  
+*/
+
+#include <stdio.h>
+  
+  int main(void) 
+  {
+    printf("Parkinson's Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+  }
+
+/*
+  (a) Identify the directives and statements in the program.
+      Directives - #include
+      Statements - 3 printf statements and 1 return statements
+  
+  (b) What output does the program produce.  
+      Parkinson's Law:
+      Work expands so as to fill the time
+      available for its completion.
+*/
+
+
