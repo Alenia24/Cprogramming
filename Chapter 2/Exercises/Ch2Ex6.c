950b575d555bbefef3655836be5c022bc012d9c0
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2022-10-20 19:31:07 -0500

    Create Ch2Ex6.c

---
+++ Chapter 2/Exercises/Ch2Ex6.c
@@ -0,0 +1,9 @@
+/*
+  Section 2.7
+  Why is it not a good idea for an identifier to contain more
+  than one adjacent underscore (as in current__balance, for example)?
+
+  Answer:
+  Identifiers that contain more than one adjacent underscore may be
+  typed incorrectly which can lead to errors.
+*/
