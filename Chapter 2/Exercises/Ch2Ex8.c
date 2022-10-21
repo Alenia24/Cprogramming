2458e4c17a5cd126924a2bcf931115632696c63f
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2022-10-20 19:41:36 -0500

    Create Ch2Ex8.c

---
+++ Chapter 2/Exercises/Ch2Ex8.c
@@ -0,0 +1,23 @@
+/*
+  Section 2.8
+  How many tokens are there in the following statement?
+  answer = (3*q-p*p)/3;
+
+  Answer:
+  The statement contains 14 tokens.
+  These are:
+  1. answer
+  2.  =
+  3.  (
+  4.  3
+  5.  *
+  6.  q
+  7.  -
+  8.  p
+  9.  *
+  10. p
+  11. )
+  12. /
+  13. 3
+  14. ;
+*/
