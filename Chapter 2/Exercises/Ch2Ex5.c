c15108c72c90d74ef882af09a9ea779c7286daaa
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2022-10-20 19:21:03 -0500

    Create Ch2Ex5.c

---
+++ Chapter 2/Exercises/Ch2Ex5.c
@@ -0,0 +1,12 @@
+/*
+Section 2.7
+Which of the following are not legal identifiers?
+(a) 100_bottles
+(b) _100_bottles
+(c) one_hundred_bottles
+(d) bottles_by_the_hundred
+
+Answer:
+(a) 100_bottles Is not a legal identifier because it begins with a digit.
+    Identifiers must begin with a letter or underscore(_).
+*/
