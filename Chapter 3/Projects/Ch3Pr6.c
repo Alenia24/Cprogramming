6bbb727bd0164441164819a2a9b416cef7809a1c
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2022-10-30 13:06:01 -0500

    Create Ch3Pr6.c

---
+++ Chapter 3/Projects/Ch3Pr6.c
@@ -0,0 +1,44 @@
+/*
+  Modify the addfrac.c program of Section 3.2 so that the user enters both
+  fractions at the same time, separated by a plus sign:
+  Enter the two fractions separated by a plus sign: 5/6+3/4
+  This sum is 38/24
+  
+  addfrac.c
+  #include <stdio.h>
+  int main(void) {
+    int num1, denom1, num2, denom2, result_num, result_denom;
+    printf("Enter the first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+    printf("Enter the second fraction: ");
+    scanf("%d/%d", &num2, &denom2);
+    result_num = num1 * denom2 + num2 * denom1;
+    result_denom = denom1 * denom2;
+    printf("The sum is %d/%d\n", result_num, result_denom);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+int main(void) {
+  int num1, denom1, num2, denom2, result_num, result_denom;
+  
+  printf("Enter the two fractions separated by a plus sign: ");
+  scanf("%d/%d+%d/%d", &num1, &denom1, &num2, &denom2);
+  
+  result_num = num1 * denom2 + num2 * denom1;
+  result_denom = denom1 * denom2;
+  
+  printf("The sum is %d/%d\n", result_num, result_denom);
+  
+  return 0;
+}
+
+/*
+  Output:
+  Enter the two fractions separated by a plus sign: 5/6+3/4
+  The sum is 38/24
+*/
+
+
+
