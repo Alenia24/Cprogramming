7603710bff1bf8584200b58fc2310f7e9af4945b
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2022-11-06 11:17:07 -0600

    Update Ch4Ex2.c

--- Chapter 4/Exercises/Ch4Ex2.c
+++ Chapter 4/Exercises/Ch4Ex2.c
@@ -2,3 +2,27 @@
 If i and j are positive integers, does (-i) / j always have the same value as -(i / j)?
 Justify your answer.
 */
+
+#include <stdio.h>
+
+int main(void) {
+  int i = 9, j = 3, result;
+
+  result = (-i) / j;
+  printf("%d\n", result);
+
+  result = -(i / j);
+  printf("%d", result);
+
+  return 0;
+}
+
+/*
+  Output: 
+  -3
+  -3
+  
+  Answer:
+   Using C89 the values would be different. Howevever, using C00 the values would be the same.
+*/
+
