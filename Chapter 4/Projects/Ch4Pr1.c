f0dd6e6d440fbe3a7d0aeb5f4bc52498e17d6daf
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2022-11-06 18:43:36 -0600

    Update Ch4Pr1.c

--- Chapter 4/Projects/Ch4Pr1.c
+++ Chapter 4/Projects/Ch4Pr1.c
@@ -1,1 +1,29 @@
+/*
+  Write a program that asks the user to enter a two-digit number, then prints
+  the number with its digits reversed. A session with the program should have
+  the following appearance:
+  Enter a two-digit number: 28
+  The reversal: 82
+*/
 
+#include <stdio.h>
+
+int main(void) {
+
+  int number;
+
+  printf("Enter a two-digit number: ");
+  scanf("%d", &number);
+
+
+
+  printf("The reversal: %d%d", number % 10, number / 10);
+
+  return 0;
+}
+
+/*
+  Output:
+  Enter a two-digit number: 28
+  The reversal: 82
+*/
