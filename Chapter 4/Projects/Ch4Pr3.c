10d836541fe46350de1ec4938704cd24ea7cfa16
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2022-11-06 20:28:31 -0600

    Create Ch4Pr3.c

---
+++ Chapter 4/Projects/Ch4Pr3.c
@@ -0,0 +1,25 @@
+/*
+  Rewite the program in Programming Project 2 so that it prints the reversal
+  of a three-digit number without using arithmetic to split the number into digits.
+  Hint: see the upc.c program of Section 4.1.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+  int n1, n2, n3;
+
+  printf("Enter a three-digit number: ");
+  scanf("%1d %1d %1d", &n1, &n2, &n3);
+
+  printf("The reversal: %d%d%d", n3, n2, n1);
+
+  return 0;
+}
+
+/*
+  Output:
+  Enter a three-digit number: 894
+  The reversal: 498
+*/
