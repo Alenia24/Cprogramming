733edcd21c3a9eb3e67acd57706e07a3544e1584
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2022-11-06 19:07:38 -0600

    Update Ch4Pr1.c

--- Chapter 4/Projects/Ch4Pr1.c
+++ Chapter 4/Projects/Ch4Pr1.c
@@ -4,20 +4,23 @@
   the following appearance:
   Enter a two-digit number: 28
   The reversal: 82
+  
+  Read the number using %d, then break it intotwo digits. Hint: If n is an integer,
+  then n % 10 is the last digit in n and n / 10 is n with the last digit removed.
 */
 
 #include <stdio.h>
 
 int main(void) {
 
-  int number;
+  int n;
 
   printf("Enter a two-digit number: ");
-  scanf("%d", &number);
+  scanf("%d", &n);
 
 
 
-  printf("The reversal: %d%d", number % 10, number / 10);
+  printf("The reversal: %d%d", n % 10, n / 10);
 
   return 0;
 }
